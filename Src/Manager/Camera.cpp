118c46809110b5ee79edd3efe97fe337b17d9122
nagatamanegidonn <158240226+nagatamanegidonn@users.noreply.github.com>
2025-02-05 17:55:37 +0900

    first commit
    初期化

---
+++ Src/Manager/Camera.cpp
@@ -0,0 +1,110 @@
+#include "../Utility/AsoUtility.h"
+#include "../Manager/InputManager.h"
+#include "Camera.h"
+
+Camera::Camera(void)
+{
+	mode_ = MODE::NONE;
+	pos_ = { 0.0f, 0.0f, 0.0f };
+	targetPos_ = { 0.0f, 0.0f, 0.0f };
+	rot_ = Quaternion::Identity();
+}
+
+Camera::~Camera(void)
+{
+}
+
+void Camera::Init(void)
+{
+	// カメラの初期設定
+	SetDefault();
+}
+
+void Camera::Update(void)
+{
+}
+
+void Camera::SetBeforeDraw(void)
+{
+
+	// クリップ距離を設定する(SetDrawScreenでリセットされる)
+	SetCameraNearFar(CAMERA_NEAR, CAMERA_FAR);
+
+	switch (mode_)
+	{
+	case Camera::MODE::FIXED_POINT:
+		SetBeforeDrawFixedPoint();
+		break;
+	case Camera::MODE::FREE:
+		SetBeforeDrawFree();
+		break;
+	}
+
+	// カメラの設定(位置と注視点による制御)
+	SetCameraPositionAndTargetAndUpVec(
+		pos_, 
+		targetPos_, 
+		cameraUp_
+	);
+
+}
+
+void Camera::SetBeforeDrawFixedPoint(void)
+{
+	// 何もしない
+}
+
+void Camera::SetBeforeDrawFree(void)
+{
+}
+
+void Camera::Draw(void)
+{
+}
+
+void Camera::Release(void)
+{
+}
+
+VECTOR Camera::GetPos(void) const
+{
+	return pos_;
+}
+
+void Camera::ChangeMode(MODE mode)
+{
+
+	// カメラの初期設定
+	SetDefault();
+
+	// カメラモードの変更
+	mode_ = mode;
+
+	// 変更時の初期化処理
+	switch (mode_)
+	{
+	case Camera::MODE::FIXED_POINT:
+		break;
+	case Camera::MODE::FREE:
+		break;
+	}
+
+}
+
+void Camera::SetDefault(void)
+{
+
+	// カメラの初期設定
+	pos_ = DEFAULT_CAMERA_POS;
+
+	// 注視点
+	targetPos_ = VAdd(pos_, RELATIVE_C2T_POS);
+
+	// カメラの上方向
+	cameraUp_ = { 0.0f, 1.0f, 0.0f };
+
+	// カメラはX軸に傾いているが、
+	// この傾いた状態を角度ゼロ、傾き無しとする
+	rot_ = Quaternion::Identity();
+
+}
